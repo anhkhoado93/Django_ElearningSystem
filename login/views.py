a489cb8ec705610906fdcf11de8710e3c68fa932
Khoa <dakhoa0903@gmail.com>
2020-12-18 23:44:30 +0700

    initial commit

---
+++ login/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .forms import UserRegistrationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.http import HttpResponse
+
+# Create your views here.
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        print(username, password)
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            redirect_url = request.GET.get('next', 'home')
+            return redirect(redirect_url)
+        else:
+            messages.error(request, "Username Or Password is incorrect!!",
+                           extra_tags='alert alert-warning alert-dismissible fade show')
+
+    return render(request, "login/login.html")
