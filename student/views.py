b319dfa2ee34eae0d63c1a0e17b9e683e96a51b1
Khoa <dakhoa0903@gmail.com>
2020-12-22 02:52:20 +0700

    submit

--- student/views.py
+++ student/views.py
@@ -9,6 +9,7 @@ from django.contrib.auth.decorators import user_passes_test
 def is_student(user):
     return user.user_type == user.USER_TYPE_CHOICES[3]
 
+# @cache_control(no_cache=True, must_revalidate=True, no_store=True) # to prevent user back button after logging out
 @login_required
 @user_passes_test(test_func=is_student,login_url= "{url accounts:login}",redirect_field_name=None)
 def homepage(request):
@@ -31,43 +32,34 @@ def aboutpage(request):
 @login_required
 @user_passes_test(test_func=is_student,login_url= "{url accounts:login}",redirect_field_name=None)
 def register(request):
+    studentId = 1852471
     if 'semester' not in request.session:
         request.session["semester"] = 201
+    submittedCourse = getEnrolledCourses(studentId=1852471, semester=request.session["semester"])
     if 'regCourse' not in request.session:
         request.session["regCourse"] = []
-    lst = getOpenedCourses(request.session["semester"])
+        if submittedCourse:
+            request.session["regCourse"] += [r['CourseId'] for r in submittedCourse]
+    openCourseList = getOpenedCourses(request.session["semester"])
     if request.method == 'POST':
         re = request.POST.get('input')
         re = re.split(' ')
         if re[0] == 'select':
             request.session["regCourse"] += [re[1]]
-            return render(request, "student/register.html", {'regCourse': request.session["regCourse"], 'openCourse': lst})
         elif re[0] == 'remove':
             request.session["regCourse"] = list(filter(lambda x: x != re[1], request.session["regCourse"]))
-            return render(request, "student/register.html", {'regCourse': request.session["regCourse"], 'openCourse': lst})
         elif re[0] == 'semester':
             request.session['semester'] = re[1]
-    return render(request, "student/register.html", {'regCourse': request.session["regCourse"],'openCourse': lst })
+        elif re[0] == 'submit':
+            for r in request.session["regCourse"]:
+                registerCourse(studentId=studentId, courseId=r, semester=request.session["semester"])
+            return render(request, "{% url 'student:home' %}")
+
+    return render(request, "student/register.html", {'regCourse': request.session["regCourse"],'openCourse': openCourseList, 'submittedCourse': submittedCourse })
 
 @login_required
 @user_passes_test(test_func=is_student,login_url= "{url accounts:login}",redirect_field_name=None)
 def course_details(request, courseId):
     studentId = 1852471
-    semester = 201
-    result_list = None
-
-    with connection.cursor() as cursor:
-        cursor.callproc('studentGetClassesAndLecturers', [studentId, semester])
-        result = cursor.fetchall()
-        for i in result:
-            if i[0] == courseId:
-                result_list = {'courseName': i[1], 'courseId': i[0], 'classId': i[2], 'lecturerName': i[3]}
-                break
-    result_list['bookTitle'] = []
-    with connection.cursor() as cursor:
-        cursor.callproc('studentGetTextbooksOfEnrolledCourses', [studentId, semester])
-        result = cursor.fetchall()
-        for i in result:
-            if i[0] == courseId:
-                result_list['bookTitle'] += [i[2]]
+    result_list = getEnrolledCourseInfo(studentId, courseId, request.session['semester'])
     return render(request, 'student/course_details.html', {'info': result_list})
