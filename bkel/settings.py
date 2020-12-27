b276219605a9fd13c1779a2e0dacc617a7e26718
hunggggg <trvhung2112@gmail.com>
2020-12-27 17:03:52 +0700

    Finish Frontend part 2

--- bkel/settings.py
+++ bkel/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'BKEL',
         'USER': 'root',
-        'PASSWORD': 'daden',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '3306',
         'OPTIONS': {
